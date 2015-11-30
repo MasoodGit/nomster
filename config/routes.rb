e013f89d3f38e8c68d993f6e2326cd557a78ab26
masoodgit <mohamed.alam78@gmail.com>
2015-11-30 04:41:15 +0000

    lesson 14, creat and redirect to places

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Nomster::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'places#index'
+  root 'places#index'
 
   resources :places
 
