186d5141e8cb1a0a6dad09a2d4efa0fc777033d8
masoodgit <mohamed.alam78@gmail.com>
2015-11-30 14:59:59 +0000

    Lesson 20,style authentication pages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Nomster::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'nomster-by-masood.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'nomster-by-masood.herokuapp.com' }
 
 end
